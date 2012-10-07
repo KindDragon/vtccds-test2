9fbe662d6cf482a077fa1b66a3dfbf1dc72b6cc6
pmiossec_cp <c676da3b177513102a5eaa395a9f94aefdca0b73cEQBKSdc>
2012-10-07 17:42:20 +0400

    script creating a branch now detect server url + accept a tfs first branch commit number (if can't detect one) + git stash to avoid errors
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS16]$/valtechgittfs/trunk;C16884

--- git-tfs-branch.sh
+++ git-tfs-branch.sh
@@ -1,15 +1,27 @@
 #script to create a branch in git-tfs
-if [ $# -ne 3 ] ; then
-  echo "parameters : ServerUrl Repository localBranch"
-  echo 'ex : "http://myTfsServer:8080/tfs/Repository" "$/MyProject/MyTFSBranch" "myBranch"'
+if [ $# -ne 2 ] && [ $# -ne 3 ] ; then
+  echo "parameters : $/ProjectRepository/BranchDirectory localBranchName [IdFirstCommitInTHeBranch]"
+  echo 'ex : "$/MyProject/MyTFSBranch" "myBranch" 345' 
   exit -1
 fi
 
+url_server=`git config --local --get tfs-remote.default.url`
+if [ -z $url_server ] ; then
+  echo "Error : No tfs/default detected"
+  exit -1
+fi
 #find TFS first commit of the branch
-let first_commit=`tf history $2 | grep "^[0-9]" | awk '{print $1}'`
+if [ $# -ne 2 ] ; then
+	let first_commit=$3
+else
+	let first_commit=`tf history $1 | grep "^[0-9]" | awk '{print $1}'`
+fi
+
 #Take the parent to have the root commit
 let root_commit=$((first_commit-1))
 echo "TFS root commit:$root_commit"
+
+git stash save
 
 #find the sha of the root parent in git log
 myCommand="git log --pretty=format:%H --grep=';C$root_commit' > git_tfs_sha.txt"
@@ -22,23 +34,23 @@ if [ -z $sha1_root_commit ] ; then
   exit -1
 fi
 #create a local branch on this commit
-git checkout -b $3 $sha1_root_commit
+git checkout -b $2 $sha1_root_commit
 
 #modify the config file to add the tfs remote
 cp -a ./.git/config ./.git/config_git_tfs_new_branch.old
 echo "
-[tfs-remote \"$3\"]
-	url = $1
-	repository = $2
-	fetch = refs/remotes/default/$3" >> ./.git/config
+[tfs-remote \"$2\"]
+	url = $url_server
+	repository = $1
+	fetch = refs/remotes/default/$2" >> ./.git/config
 
 #create the remote file to join the commit to the trunk
-echo $sha1_root_commit > ./.git/refs/remotes/tfs/$3
-#git rev-parse $3 >> ./.git/refs/remotes/tfs/$3
+echo $sha1_root_commit > ./.git/refs/remotes/tfs/$2
+#git rev-parse $2 >> ./.git/refs/remotes/tfs/$2
 
 #fetch commits
 echo "Fetching tfs commits..."
-git tfs fetch -i $3
+git tfs fetch -i $2
 #reset the local branch to the head of the 
-git reset --soft tfs/$3
+git reset --soft tfs/$2
 rm git_tfs_sha.txt
