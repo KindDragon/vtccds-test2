72ac84370d5ef74dd3838ae13172c3cba6f00ec3
pmiossec_cp <c676da3b177513102a5eaa395a9f94aefdca0b73cEQBKSdc>
2012-10-14 00:19:15 +0400

    script : meilleure gestion de la recherche du commit parent + correction expression rationnelle!
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS16]$/valtechgittfs/trunk;C17024

--- git-tfs-branch.sh
+++ git-tfs-branch.sh
@@ -2,7 +2,7 @@
 if [ $# -ne 3 ] ; then
   echo "parameters : $/ProjectRepository/BranchDirectory localBranchName $/ProjectRepository/SourceBranchDirectory"
   echo 'ex : "$/MyProject/MyTFSBranch" "myBranch" "$/MyProject/MyTFSTrunk"' 
-  echo "parameters : $/ProjectRepository/BranchDirectory localBranchName [IdParentCommitBeforeBranching]"
+  echo "parameters : $/ProjectRepository/BranchDirectory localBranchName IdParentCommitBeforeBranching"
   echo 'ex : "$/MyProject/MyTFSBranch" "myBranch" 345' 
   exit -1
 fi
@@ -12,35 +12,44 @@ if [ ! -z "$(echo $2 | sed -n 's/\([.\/]\)/\1/p')" ]; then
 	exit -1
 fi
 
+#find TFS first commit of the branch
+if [[ $3 != *[!0-9]* ]]; then
+	let root_commit=$3
+	echo "TFS root commit:$root_commit"
+  else
+	let root_commit=`tf history $1 | grep "^[0-9]" | awk '{print $1}'`
+	if [ -z $root_commit ] ; then
+		echo "Error executing commant 'tf' :( Remember that the script must be run from a TFS mapped directory ! (Thank you TFS!)"
+		exit -1
+	fi
+	echo $root_commit
+	#Take the parent to have the root commit
+	while [ $root_commit -ne 0 ] && [ -z $sha1_root_commit ] ; do
+		root_commit=$((root_commit-1))
+		#echo "try to find if changeset $root_commit is the good one" 
+		sha1_root_commit=$(git log --pretty=format:%H --grep="$3;C$root_commit[^0-9]")
+		#echo "Is Git sha root commit:$sha1_root_commit"
+	done
+	if [ $root_commit -eq 0 ] ; then
+		echo "The root changset not founded!"
+		exit -1
+	fi
+	echo "The root changset founded id is $root_commit !"
+	echo "Please verify that it's the good changset in the TFS history before calling the script with the parameters:"
+	echo "$1 $2 $root_commit"
+	exit 0
+fi
+
 url_server=`git config --local --get tfs-remote.default.url`
 if [ -z $url_server ] ; then
   echo "Error : No tfs/default detected. This repository is not in the good state!"
   exit -1
 fi
 
-#find TFS first commit of the branch
-if [[ $3 != *[!0-9]* ]]; then
-	let root_commit=$3
-	echo "TFS root commit:$root_commit"
-  else
-	let first_commit=`tf history $1 | grep "^[0-9]" | awk '{print $1}'`
-	#Take the parent to have the root commit
-	let root_commit=$((first_commit-1))
-	echo "The root changset founded id is $root_commit !"
-	echo "Please verify that it's the good changset beafore calling the script with the parameters:"
-	echo "$1 $2 $root_commit"
-	exit 0
-fi
-
-exit -1
-
 #find the sha of the root parent in git log
-myCommand="git log --pretty=format:%H --grep=';C$root_commit' > git_tfs_sha.txt"
-eval $myCommand
-sha1_root_commit=$(<git_tfs_sha.txt)
-rm git_tfs_sha.txt
-
+sha1_root_commit=$(git log --pretty=format:%H --grep=";C$root_commit[^0-9]")
 echo "Git root commit:$sha1_root_commit"
+
 if [ -z $sha1_root_commit ] ; then
   echo "Error during detecting commit root :("
   exit -1
